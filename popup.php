c5fe46f06358de0a373daaaa77107a4fc93974df
Felipe <81455073+felipesantos2@users.noreply.github.com>
2023-05-29 19:56:21 -0300

    Update and rename social_embed.php to popup.php

---
+++ popup.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Popup
+ * Plugin URI:https://github.com/felipesantos2/Popup
+ * Description: Social video embed.
+ * Version: 1.0.0
+ * Author: Felipe Pinheiro
+ * Author URI: https://github.com/felipesantos2
+ */
+
+/* Main Plugin File */
+
+    
